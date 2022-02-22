325b4e1f9ffe6426a3a3957b8bda8cbd76f922a0
mikeeq <miotk.mikolaj@gmail.com>
2022-02-22 19:33:41 +0100

    update build.sh v5

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -6,6 +6,10 @@ SCRIPT_PATH=${SCRIPT_PATH:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
 echo "===]> Info: RUN_PATH=$RUN_PATH"
 echo "===]> Info: SCRIPT_PATH=$SCRIPT_PATH"
 
+pwd
+echo "CPU threads: $(nproc --all)"
+grep 'model name' /proc/cpuinfo | uniq
+
 USERID=1000
 GROUPID=1000
 
@@ -15,7 +19,8 @@ apt-get update && \
 apt-get install -y time git-core subversion build-essential ccache ecj fastjar file screen quilt libncursesw5-dev libssl-dev \
   g++ java-propose-classpath libelf-dev bash make patch libncurses5 libncurses5-dev zlib1g-dev gawk \
   flex gettext wget unzip xz-utils python python-distutils-extra python3 python3-distutils-extra rsync \
-  python3-setuptools python3-dev swig xsltproc zlib1g-dev llvm clang nano sudo && \
+  python3-setuptools python3-dev swig xsltproc zlib1g-dev llvm clang nano \
+    sudo curl && \
 apt-get clean && \
 groupadd --gid $GROUPID buser && \
 useradd --uid $USERID --gid $GROUPID -m -s /bin/bash buser

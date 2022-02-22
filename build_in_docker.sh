0fb80f39ac8ae4ae8721f9f1f8467753e1f8aa38
mikeeq <miotk.mikolaj@gmail.com>
2022-02-22 21:56:49 +0100

    test ubuntu:21.10 build

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -18,19 +18,19 @@ chown -R $USERID:$GROUPID $SCRIPT_PATH
 apt-get update && \
 apt-get install -y sudo curl vim gnupg
 
-echo "
-deb http://apt.llvm.org/buster/ llvm-toolchain-buster-12 main
-deb-src http://apt.llvm.org/buster/ llvm-toolchain-buster-12 main" >> /etc/apt/sources.list
+# echo "
+# deb http://apt.llvm.org/buster/ llvm-toolchain-buster-12 main
+# deb-src http://apt.llvm.org/buster/ llvm-toolchain-buster-12 main" >> /etc/apt/sources.list
 
-curl -Ls https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+# curl -Ls https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 
-export CMAKE_C_COMPILER=clang-12
-export CMAKE_CXX_COMPILER=clang++-12
+# export CMAKE_C_COMPILER=clang-12
+# export CMAKE_CXX_COMPILER=clang++-12
 
 apt-get update && \
 apt-get install -y time git-core subversion build-essential ccache ecj fastjar file screen quilt libncursesw5-dev libssl-dev \
   g++ java-propose-classpath libelf-dev bash make patch libncurses5 libncurses5-dev zlib1g-dev gawk \
-  flex gettext wget unzip xz-utils python python-distutils-extra python3 python3-distutils-extra rsync \
+  flex gettext wget unzip xz-utils python3 python3-distutils-extra rsync \
   python3-setuptools python3-dev swig xsltproc zlib1g-dev llvm clang-12 && \
 apt-get clean && \
 groupadd --gid $GROUPID buser && \

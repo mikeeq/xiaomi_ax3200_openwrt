1b766d37f42f2617703e397963e2033dab4555dd
mikeeq <miotk.mikolaj@gmail.com>
2022-03-04 12:09:33 +0100

    bump openwrt version v3

--- build_image/run_build_image_in_docker.sh
+++ build_image/run_build_image_in_docker.sh
@@ -25,7 +25,7 @@ inf "SKIP_PULL=$SKIP_PULL"
 inf "BUILD_VERBOSE=$BUILD_VERBOSE"
 inf "SKIP_DOWNLOAD=$SKIP_DOWNLOAD"
 
-docker build -t openwrt .
+# docker build -t openwrt .
 
 # docker pull ${DOCKER_IMAGE}
 if [[ $RUN_SHELL == true ]]; then
